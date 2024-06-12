47548f14665fbd79b1750dc75d173368f73d03cb
mikepapadim <mikepapadim@hotmail.com>
2024-06-12 17:21:33 +0100

    wip

---
+++ docker-uniman/run_bdt_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script builds a Docker image, runs a Docker container with GPU support, and executes a specific example inside the container automatically.
+
+docker run --gpus all -it openfhe-uniman-gpu-cuda ./build/bin/examples/pke/bdt_server_with_treebuilder
+
