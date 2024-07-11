f330852dd711c4261ef28bd2ca8bd343e9c519dd
Thanos Stratikopoulos <34061419+stratika@users.noreply.github.com>
2024-07-11 13:23:35 +0300

    Update docker container in run_bdt_server.sh

--- docker-uniman/run_bdt_server.sh
+++ docker-uniman/run_bdt_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script builds a Docker image, runs a Docker container with GPU support, and executes a specific example inside the container automatically.
 
-docker run --gpus all -it openfhe-uniman-gpu-cuda ./build/bin/examples/pke/bdt_server_with_treebuilder
+docker run --gpus all -it beehivelab/openfhe-uniman-gpu-cuda:latest ./build/bin/examples/pke/bdt_server_with_treebuilder
 
