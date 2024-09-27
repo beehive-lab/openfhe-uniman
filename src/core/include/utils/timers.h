8103b0a9ec36c0cc77f4d42c814895b03fe9d85a
Orion Papadakis <orion.papadakis@manchester.ac.uk>
2024-09-27 12:13:40 +0300

    [feat] Add new timers

--- src/core/include/utils/timers.h
+++ src/core/include/utils/timers.h
@@ -7,11 +7,17 @@
 
 namespace lbcrypto {
 
-extern double approxSwitchTimer;
+extern double approxSwitchTimer_CPU;
+extern double approxSwitchTimer_GPU;
+extern double approxModDownTimer_CPU;
+extern double approxModDownTimer_GPU;
 
 void accumulateTimer(double &timer, double toc);
 
-double getApproxSwitchTimer();
+double getApproxSwitchTimerCPU();
+double getApproxSwitchTimerGPU();
+double getApproxModDownTimerCPU();
+double getApproxModDownTimerGPU();
 
 void printTimers();
 
--- src/core/lib/utils/timers.cpp
+++ src/core/lib/utils/timers.cpp
@@ -11,18 +11,36 @@
 
 namespace lbcrypto {
 
-double approxSwitchTimer = 0;
+double approxSwitchTimer_CPU = 0;
+double approxSwitchTimer_GPU = 0;
+double approxModDownTimer_CPU = 0;
+double approxModDownTimer_GPU = 0;
 
 void accumulateTimer(double &timer, double toc) {
     timer += toc;
 }
 
-double getApproxSwitchTimer() {
-    return approxSwitchTimer;
+double getApproxSwitchTimerCPU() {
+    return approxSwitchTimer_CPU;
+}
+
+double getApproxSwitchTimerGPU() {
+    return approxSwitchTimer_GPU;
+}
+
+double getApproxModDownTimerCPU() {
+    return approxSwitchTimer_CPU;
+}
+
+double getApproxModDownTimerGPU() {
+    return approxModDownTimer_GPU;
 }
 
 void printTimers() {
-    std::cout << "Total time in ApproxSwitchCRTBasis = " << getApproxSwitchTimer() << "ms" << std::endl;
+    std::cout << "Total time in ApproxSwitchCRTBasis_CPU = " << getApproxSwitchTimerCPU() << "ms" << std::endl;
+    std::cout << "Total time in ApproxSwitchCRTBasis_GPU = " << getApproxSwitchTimerGPU() << "ms" << std::endl;
+    std::cout << "Total time in ApproxModDown_CPU = " << getApproxModDownTimerCPU() << "ms" << std::endl;
+    std::cout << "Total time in ApproxModDown_GPU = " << getApproxModDownTimerGPU() << "ms" << std::endl;
 }
 
 };
