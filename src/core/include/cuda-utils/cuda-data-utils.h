338fba42bf0b9ffcc92f064b54fd251975581fc8
Orion Papadakis <orion.papadakis@manchester.ac.uk>
2024-11-15 13:20:27 +0200

    [feat] Add and refactor macros for safety checks

--- src/core/include/cuda-utils/cuda-data-utils.h
+++ src/core/include/cuda-utils/cuda-data-utils.h
@@ -14,14 +14,34 @@ namespace lbcrypto {
  *  A macro that checks the return value of CUDA calls and reports errors.
  * @param call
  */
-#define CUDA_CHECK(call) do {                                    \
-    cudaError_t err = call;                                      \
-    if (err != cudaSuccess) {                                    \
-        fprintf(stderr, "CUDA Error at %s:%d - %s\n",            \
-                __FILE__, __LINE__, cudaGetErrorString(err));    \
-        exit(EXIT_FAILURE);                                      \
-    }                                                            \
-} while (0)
+#define CUDA_CHECK(call)                                             \
+    do {                                                             \
+        cudaError_t err = call;                                      \
+        if (err != cudaSuccess) {                                    \
+            fprintf(stderr, "CUDA Error at %s:%d - %s\n",            \
+                    __FILE__, __LINE__, cudaGetErrorString(err));    \
+            exit(EXIT_FAILURE);                                      \
+        }                                                            \
+    } while (0)
+
+#define CUDA_SAFE_FREE(ptr, stream)                                  \
+    do {                                                             \
+        if ((ptr) != nullptr) {                                      \
+            cudaError_t err = cudaFreeAsync((ptr), (stream));        \
+            if (err != cudaSuccess) {                                \
+                fprintf(stderr, "CUDA Error at %s:%d - %s\n",        \
+                        __FILE__, __LINE__, cudaGetErrorString(err));\
+                exit(EXIT_FAILURE);                                  \
+            }                                                        \
+            (ptr) = nullptr;                                         \
+        }                                                            \
+    } while (0)
+
+#define SAFE_FREE(ptr) \
+    do {               \
+        free(ptr);     \
+        ptr = NULL;    \
+    } while (0)
 
 using PolyType = PolyImpl<NativeVector>;
 
